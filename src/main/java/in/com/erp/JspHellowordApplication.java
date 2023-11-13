2e8e558706df8f54aa34e3b9719926ac5be3666d
mayur <mayurtitirmare261@gmail.com>
2023-11-13 21:08:59 +0530

    Jsp spring boot example

---
+++ src/main/java/in/com/erp/JspHellowordApplication.java
@@ -0,0 +1,13 @@
+package in.com.erp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JspHellowordApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JspHellowordApplication.class, args);
+	}
+
+}
---
+++ src/test/java/in/com/erp/JspHellowordApplicationTests.java
@@ -0,0 +1,13 @@
+package in.com.erp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JspHellowordApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
